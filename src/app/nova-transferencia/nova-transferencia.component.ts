e8a6e00f0549feb2fbb9115ee32a5068b2ffe7ae
Lucas Martins <lucasifbaeuna@gmail.com>
2022-04-11 14:56:31 -0300

    feat: criação do layout ByteBank

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,39 @@
+import { TransferenciaService } from './../services/transferencia.service';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Transferencia } from '../models/transferencia.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent implements OnInit {
+
+  @Output()
+  aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  constructor(private transferenciaService:TransferenciaService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  transferir(){
+    console.log('solicitar nova transferencia');
+    const valueEmmitter: Transferencia = {valor:this.valor, destino:this.destino}
+    this.transferenciaService.adicionar(valueEmmitter).subscribe(transferirSucess => {
+      console.log(transferirSucess);
+      this.limparCampos();
+      this.router.navigateByUrl('extrato');
+
+    }, error => console.error(error));
+  }
+
+  private limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
